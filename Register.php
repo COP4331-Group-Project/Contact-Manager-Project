6c9caf99e83f3bf21feb73634615dd8632944c18
Qvanlom <59709023+Qvanlom@users.noreply.github.com>
2023-09-04 16:15:32 -0400

    Update and rename AddColor.php to Register.php
    Register now works

--- Register.php
+++ Register.php
@@ -1,22 +1,30 @@
 <?php
 	$inData = getRequestInfo();
-	
-	$color = $inData["color"];
-	$userId = $inData["userId"];
-
-	$conn = new mysqli("localhost", "root", "Cop4331Group23FTW", "COP4331"); 
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$login = $inData["Login"];
+	$password = $inData["Password"];
+	echo "Zone2";
+	$conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
+	echo "Zone3";
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
+		echo "Zone4";
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+		echo "Zone5";
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		echo "Zone6";
 		$stmt->execute();
+		echo "Zone7";
 		$stmt->close();
 		$conn->close();
+		echo "Zone8";
 		returnWithError("");
+		echo "Zone9";
 	}
 
 	function getRequestInfo()
