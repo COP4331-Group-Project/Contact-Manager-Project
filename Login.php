8b7329f9ff505da1cd1dd005a0bc26d0ca2eccb6
Qvanlom <59709023+Qvanlom@users.noreply.github.com>
2023-09-04 16:14:57 -0400

    Update Login.php
    Changed lowercase titles to uppercase

--- Login.php
+++ Login.php
@@ -16,7 +16,7 @@
 	else
 	{
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
