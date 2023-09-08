88af698b4ac5241b9d00f46bf82bb1b5820e7ffe
Qvanlom <59709023+Qvanlom@users.noreply.github.com>
2023-09-08 15:46:42 -0400

    Update Register.php
    Removed debugging information

--- Register.php
+++ Register.php
@@ -4,27 +4,22 @@
 	$lastName = $inData["LastName"];
 	$login = $inData["Login"];
 	$password = $inData["Password"];
-	echo "Zone2";
+
 	$conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
-	echo "Zone3";
+
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		echo "Zone4";
 		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		echo "Zone5";
 		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-		echo "Zone6";
+
 		$stmt->execute();
-		echo "Zone7";
 		$stmt->close();
 		$conn->close();
-		echo "Zone8";
 		returnWithError("");
-		echo "Zone9";
 	}
 
 	function getRequestInfo()
