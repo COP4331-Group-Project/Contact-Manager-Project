88caeae705c9f9e80df7c0536a0ec04891982101
vincent desk <vincentdp02@gmail.com>
2023-09-11 14:26:47 -0400

    updated more css

--- ContactList.js
+++ ContactList.js
@@ -3,14 +3,16 @@ let addContactForm = document.getElementById("addContact");
 let btn = document.getElementById("btn");
 let inputGroup = document.querySelector('.inputWrapper');
 
-const search = () => {
+const search = () => 
+{
   searchForm.style.left = "50px";
   addContactForm.style.left ="450px";
   btn.style.left = "0px";
   inputGroup.style.gridTemplateColumns = '1fr 1fr';
 }
 
-const addContact = () => {
+const addContact = () => 
+{
   searchForm.style.left = "-400px";
   addContactForm.style.left ="50px";
   btn.style.left = "110px";
