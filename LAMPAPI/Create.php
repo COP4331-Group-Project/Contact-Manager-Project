b48af3de34e4600e901957ffff4791ae10f18a4a
cbreitbart <143041270+cbreitbart@users.noreply.github.com>
2023-09-14 16:22:48 -0400

    Update Create.php
    Updated to access the session and retrieve the session variable from when the user Logged in and their ID gets stored as the session variable.

--- LAMPAPI/Create.php
+++ LAMPAPI/Create.php
@@ -1,42 +1,64 @@
 <?php
-	$inData = getRequestInfo();
-	$firstName = $inData["FirstName"];
-	$lastName = $inData["LastName"];
-	$phone = $inData["Phone"];
-	$email = $inData["Email"];
-	$userId = $inData["UserID"];
-	
-	$conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
+    session_start(); // Start a PHP session (if not already started)
+    $inData = getRequestInfo();
 
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $userId);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+    $firstName = $inData["FirstName"];
+    $lastName = $inData["LastName"];
+    $phone = $inData["Phone"];
+    $email = $inData["Email"];
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+    // Check if the user is authenticated and the UserID is available in the session
+    if (isset($_SESSION["UserID"])) 
+    {
+        $loggedInUserID = $_SESSION["UserID"];
+    } 
+    else
+    {
+        // Handle the case when the user is not authenticated
+        returnWithError("User not authenticated.");
+        exit(); // Stop execution if the user is not authenticated
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    $conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
+
+    if ($conn->connect_error) 
+    {
+        returnWithError($conn->connect_error);
+    } 
+    else
+    {
+        $stmt = $conn->prepare("INSERT INTO Contacts (FirstName, LastName, Phone, Email, UserID) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $loggedInUserID);
+   
+        if ($stmt->execute()) 
+        {
+            // Contact record inserted successfully
+            $stmt->close();
+            $conn->close();
+            returnWithError("");
+        } 
+        else 
+        {
+            // Handle any errors that occur during the insertion
+            returnWithError("Error inserting contact.");
+        }
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+    
 ?>
