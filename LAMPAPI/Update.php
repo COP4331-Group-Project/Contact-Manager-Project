0d3b955b09e21b47b9082b41369271de850f50f9
Qvanlom <59709023+Qvanlom@users.noreply.github.com>
2023-09-14 05:33:29 -0400

    Add update.php
    Currently can update first name using the ID number

---
+++ LAMPAPI/Update.php
@@ -0,0 +1,40 @@
+<?php
+	$inData = getRequestInfo();
+	$firstName = $inData["FirstName"];
+    $id = $inData["ID"];
+
+	$conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=? WHERE ID=?");
+		$stmt->bind_param("ss", $firstName, $id);
+
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
