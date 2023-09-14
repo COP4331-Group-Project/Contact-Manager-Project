cf58de4506d1dbdbdf4687cd2a6fcb9f2d808d45
Qvanlom <59709023+Qvanlom@users.noreply.github.com>
2023-09-14 04:48:52 -0400

    Add create.php
    Works. Except userID needs to be inputted manually for now

---
+++ LAMPAPI/Create.php
@@ -0,0 +1,46 @@
+<?php
+	echo "Zone1";
+	$inData = getRequestInfo();
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$phone = $inData["Phone"];
+	$email = $inData["Email"];
+	$userId = $inData["UserID"];
+	
+	$conn = new mysqli("localhost", "Admin", "Dev123", "COP4331"); 
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		echo "Zone2";
+
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		echo "Zone3";
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
