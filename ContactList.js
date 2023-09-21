83dc571171ef75a6aba4f069748d4844ebca2b73
vincent <vincentdp02@gmail.com>
2023-09-21 14:38:19 -0400

    Changed the entire contact table page

--- ContactList.js
+++ ContactList.js
@@ -1,23 +1,5 @@
-let searchForm = document.getElementById("search");
-let addContactForm = document.getElementById("addContact");
-let btn = document.getElementById("btn");
-let inputGroup = document.querySelector('.inputWrapper');
-
-const search = () => 
-{
-  searchForm.style.left = "50px";
-  addContactForm.style.left ="450px";
-  btn.style.left = "0px";
-  inputGroup.style.gridTemplateColumns = '1fr 1fr';
-}
-
-const addContact = () => 
-{
-  searchForm.style.left = "-400px";
-  addContactForm.style.left ="50px";
-  btn.style.left = "110px";
-  inputGroup.style.gridTemplateColumns = "repeat(2, 1fr)";
-}
+const modalIdToClose = ['editModal', 'addContactModal']
+let editRowIndex = -1; // Track the row being edited
 
 function deleteRow(button) {
   var row = button.parentNode.parentNode;
@@ -29,49 +11,91 @@ function deleteRow(button) {
   }
 }
 
-var editRowIndex = -1; // Track the row being edited
+function editRow(button) {
 
-    function editRow(button) {
-      // Get the dialog element
-      const editForm = document.getElementById("editForm");
-      
-      // Get the row index
-      var row = button.parentNode.parentNode;
-      editRowIndex = row.rowIndex;
+  // Get the row index
+  var row = button.parentNode.parentNode;
+  editRowIndex = row.rowIndex;
 
-      // Display the edit form
-      // document.getElementById('editForm').style.display = 'flex';
-      editForm.showModal();
+  // Populate the form with row data
+  document.getElementById('editFirstName').value = button.getAttribute('data-firstname');
+  document.getElementById('editLastName').value = button.getAttribute('data-lastname');
+  document.getElementById('editEmail').value = button.getAttribute('data-email');
+  document.getElementById('editPhone').value = button.getAttribute('data-phone');
 
-      // Populate the form with row data
-      document.getElementById('editFirstName').value = button.getAttribute('data-firstname');
-      document.getElementById('editLastName').value = button.getAttribute('data-lastname');
-      document.getElementById('editEmail').value = button.getAttribute('data-email');
-      document.getElementById('editPhone').value = button.getAttribute('data-phone');
-    }
+  // Open the modal
+  const modalId = button.getAttribute('data-modal-id');
+  openModal(modalId);
+  
+}
 
-    function updateRow() {
-      // Update the table row with the edited data
-      var table = document.querySelector('table');
-      var row = table.rows[editRowIndex];
-      row.cells[0].textContent = document.getElementById('editFirstName').value;
-      row.cells[1].textContent = document.getElementById('editLastName').value;
-      row.cells[2].textContent = document.getElementById('editEmail').value;
-      row.cells[3].textContent = document.getElementById('editPhone').value;
+function updateRow() 
+{
+  // Update the table row with the edited data
+  var table = document.querySelector('table');
+  var row = table.rows[editRowIndex];
+  row.cells[0].textContent = document.getElementById('editFirstName').value;
+  row.cells[1].textContent = document.getElementById('editLastName').value;
+  row.cells[2].textContent = document.getElementById('editEmail').value;
+  row.cells[3].textContent = document.getElementById('editPhone').value;
 
-      // Close the dialog
-      const editForm = document.getElementById("editForm");
-      editForm.close();
-
-      // Reset the editRowIndex
-      editRowIndex = -1;
-    }
-
-function closeModal() {
-  // Close the dialog without saving changes
+  // Close the dialog
   const editForm = document.getElementById("editForm");
   editForm.close();
-    
+
   // Reset the editRowIndex
   editRowIndex = -1;
 }
+
+// ADD CONTACT FUNCTIONS
+const addContact = () => 
+{
+  // Open Modal
+  const addContactModalId= document.getElementById('addContactModal');
+  addContactModalId.showModal();
+}
+
+// MODAL FUNCTIONS
+function closeModal(id) {
+  const modal = document.getElementById(id);
+  modal.close();
+  // Reset the editRowIndex
+  editRowIndex = -1;
+}
+
+const openModal = (id) => 
+{
+  const modal = document.getElementById(id);
+  // Open the modal
+  modal.showModal();
+}
+
+// Closing the edit modal
+const editModal = document.getElementById('editModal');
+editModal.addEventListener('click', (event) => 
+{
+  const dialogDimensions = editModal.getBoundingClientRect();
+  if (
+    event.clientX < dialogDimensions.left ||
+    event.clientX > dialogDimensions.right || 
+    event.clientY < dialogDimensions.top ||
+    event.clientY > dialogDimensions.bottom
+  ) {
+    editModal.close();
+  }
+})
+
+// Close the Add Contact Modal
+const contactModal = document.getElementById('addContactModal');
+contactModal.addEventListener('click', (event) => 
+{
+  const dialogDimensions = contactModal.getBoundingClientRect();
+  if (
+    event.clientX < dialogDimensions.left ||
+    event.clientX > dialogDimensions.right || 
+    event.clientY < dialogDimensions.top ||
+    event.clientY > dialogDimensions.bottom
+  ) {
+    contactModal.close();
+  }
+})
