691db65f15a10b9e2c1deec2dcba74bf7d13c3f1
austiniza <142841858+austiniza@users.noreply.github.com>
2023-09-24 14:02:06 -0400

    Added search and add contact function

--- js/ContactList.js
+++ js/ContactList.js
@@ -99,3 +99,91 @@ contactModal.addEventListener('click', (event) =>
     contactModal.close();
   }
 })
+
+function add()
+{
+  let firstName = document.getElementById('newFirstName').value.trim();
+  let lastName = document.getElementById('newLastName').value.trim(); 
+  let email = document.getElementById('newEmail').value.trim(); 
+  let number = document.getElementById('newPhoneNum').value.trim(); 
+
+	document.getElementById("contactAddResult").innerHTML = "";
+
+	let tmp = 
+  {
+    FirstName: newFirstName,
+    LastName: newLastName,
+    Phone: newPhoneNum,
+    Email: newEmail,
+    UserID: userId,
+  };
+
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/Create.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactAddResult").innerHTML = err.message;
+	}
+	
+}
+
+function searchContact()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
+	
+	let contactList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/Search.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					contactList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						contactList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+	
+}
